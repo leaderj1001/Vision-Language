88978fb51a434c5cda16928725c361397828da3f
swdsld <40786892+swdsld@users.noreply.github.com>
2019-05-01 23:59:02 +0900

    Update main.py
    question loader modified

--- GQA/main.py
+++ GQA/main.py
@@ -3,13 +3,12 @@ import numpy as np
 from tqdm import tqdm
 
 from model import Model
-from preprocess import get_filename, load_question, batch_iterator
+from preprocess import get_filename, load_all_question, batch_iterator
 from config import get_args
 
 
 def main():
     args = get_args()
-    train_filenames, test_filenames = get_filename()
 
     with tf.Session() as sess:
         model = Model(sess, [224, 224, 3], image_pretrained="densenet")
@@ -19,21 +18,20 @@ def main():
         # img = model.image_model.preprocess(image_test_x)
 
         for epoch in range(1, args.epochs):
-            for train_filename in train_filenames:
-                train_question_json = load_question(train_filename)
+            train_questions, test_questions = load_all_question()
 
-                train_correct = 0
-                for batch_image, batch_question, batch_question_length, batch_answer in tqdm(batch_iterator(train_question_json, args.batch_size)):
-                    _, y_pred, loss = sess.run([model.train, model.y_pred, model.loss], feed_dict={
-                        model.image_input: batch_image,
-                        model.question_input: batch_question,
-                        model.question_length: batch_question_length,
-                        model.target: batch_answer
-                    })
+            train_correct = 0
+            for batch_image, batch_question, batch_question_length, batch_answer in tqdm(batch_iterator(train_questions, args.batch_size)):
+                _, y_pred, loss = sess.run([model.train, model.y_pred, model.loss], feed_dict={
+                    model.image_input: batch_image,
+                    model.question_input: batch_question,
+                    model.question_length: batch_question_length,
+                    model.target: batch_answer
+                })
 
-                    print(train_correct, loss)
-                    train_correct += (np.argmax(y_pred) == np.argmax(batch_answer)).sum()
-                    print(train_correct, loss)
+                print(train_correct, loss)
+                train_correct += (np.argmax(y_pred) == np.argmax(batch_answer)).sum()
+                print(train_correct, loss)
 
             # for test_filename in test_filenames:
             #     test_question_json = load_question(test_filename)
