64749a8f7e0adcb726936ae3781eb6b6ba0721f5
swdsld <40786892+swdsld@users.noreply.github.com>
2019-05-02 23:07:02 +0900

    Update main.py

--- GQA/main.py
+++ GQA/main.py
@@ -9,7 +9,7 @@ from config import get_args
 
 def main():
     args = get_args()
-
+    saver = tf.train.Saver()
     with tf.Session() as sess:
         model = Model(sess, [224, 224, 3], image_pretrained="densenet")
         sess.run(tf.global_variables_initializer())
@@ -30,6 +30,7 @@ def main():
                 hit = np.array(np.argmax(y_pred, axis=1) == np.argmax(batch_answer, axis=1))
                 train_acc = np.sum(hit) / hit.shape[0]
                 print('epoch ' + str(epoch) + ' train_acc: ' + str(train_acc) + ' loss: ' + str(loss))
+            saver.save('./checkpoint/vision_lang', global_step=model.global_step)
             # for test_filename in test_filenames:
             #     test_question_json = load_question(test_filename)
             #     for batch_image, batch_question, batch_question_length, answer in tqdm(batch_iterator(test_question_json, args.batch_size)):
