d0a33a1b8bdb0c79111516728a30a5f18268899c
swdsld <40786892+swdsld@users.noreply.github.com>
2019-05-02 13:59:35 +0900

    Update main.py
    modify accuracy measure

--- GQA/main.py
+++ GQA/main.py
@@ -20,19 +20,16 @@ def main():
         for epoch in range(1, args.epochs):
             train_questions, test_questions = load_all_question()
 
-            train_correct = 0
-            for batch_image, batch_question, batch_question_length, batch_answer in tqdm(batch_iterator(train_questions, args.batch_size)):
+            for batch_image, batch_question, batch_question_length, batch_answer in batch_iterator(train_questions, args.batch_size):
                 _, y_pred, loss = sess.run([model.train, model.y_pred, model.loss], feed_dict={
                     model.image_input: batch_image,
                     model.question_input: batch_question,
                     model.question_length: batch_question_length,
                     model.target: batch_answer
                 })
-
-                print(train_correct, loss)
-                train_correct += (np.argmax(y_pred) == np.argmax(batch_answer)).sum()
-                print(train_correct, loss)
-
+                hit = np.array(np.argmax(y_pred, axis=1) == np.argmax(batch_answer, axis=1))
+                train_acc = np.sum(hit) / hit.shape[0]
+                print('epoch ' + str(epoch) + ' train_acc: ' + str(train_acc) + ' loss: ' + str(loss))
             # for test_filename in test_filenames:
             #     test_question_json = load_question(test_filename)
             #     for batch_image, batch_question, batch_question_length, answer in tqdm(batch_iterator(test_question_json, args.batch_size)):
