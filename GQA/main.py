02e87ac0887a0741771ebb75cb8e26bdcf3a414f
swdsld <taehoon1018@postech.ac.kr>
2019-05-07 14:33:18 +0900

    Update main.py
    Saving every 1000 iterations. Logging global step

--- GQA/main.py
+++ GQA/main.py
@@ -1,41 +1,70 @@
 import tensorflow as tf
 import numpy as np
-from tqdm import tqdm
+import os
+import time
 
 from model import Model
-from preprocess import get_filename, load_all_question, batch_iterator
+from preprocess import load_all_question, batch_iterator
 from config import get_args
 
+# Image
+# img = model.image_model.preprocess(image_test_x)
 
-def main():
-    args = get_args()
+
+def main(args):
+    os.environ['CUDA_VISIBLE_DEVICES'] = "0"
     saver = tf.train.Saver()
     with tf.Session() as sess:
+        merged = tf.summary.merge_all()
+        if not os.path.isdir('reporting'):
+            os.mkdir('reporting')
+        writer = tf.summary.FileWriter('./reporting', sess.graph)
+
         model = Model(sess, [224, 224, 3], image_pretrained="densenet")
         sess.run(tf.global_variables_initializer())
+        sess.run(model.global_step)
 
-        # Image
-        # img = model.image_model.preprocess(image_test_x)
-
+        # start_time = time.time()
         for epoch in range(1, args.epochs):
             train_questions, test_questions = load_all_question()
 
             for batch_image, batch_question, batch_question_length, batch_answer in batch_iterator(train_questions, args.batch_size):
-                _, y_pred, loss = sess.run([model.train, model.y_pred, model.loss], feed_dict={
+                gstep, _, train_loss, train_accuracy, t_l_summary, t_a_summary = sess.run([model.global_step, model.train_op, model.loss, model.accuracy, model.train_loss_summary, model.train_acc_summary], feed_dict={
                     model.image_input: batch_image,
                     model.question_input: batch_question,
                     model.question_length: batch_question_length,
                     model.target: batch_answer
                 })
-                hit = np.array(np.argmax(y_pred, axis=1) == np.argmax(batch_answer, axis=1))
-                train_acc = np.sum(hit) / hit.shape[0]
-                print('epoch ' + str(epoch) + ' train_acc: ' + str(train_acc) + ' loss: ' + str(loss))
-            saver.save('./checkpoint/vision_lang', global_step=model.global_step)
-            # for test_filename in test_filenames:
-            #     test_question_json = load_question(test_filename)
-            #     for batch_image, batch_question, batch_question_length, answer in tqdm(batch_iterator(test_question_json, args.batch_size)):
-            #         pass
+                # hit = np.array(np.argmax(y_pred, axis=1) == np.argmax(batch_answer, axis=1))
+                # train_acc = np.sum(hit) / hit.shape[0]
+                print('gstep: ' + str(gstep) + ' epoch ' + str(epoch) + ' train_acc: ' + str(train_accuracy) + ' loss: ' + str(train_loss))
+
+                writer.add_summary(t_l_summary, gstep)
+                writer.add_summary(t_a_summary, gstep)
+                if gstep % 1000 == 0:
+                    print('save to checkpoint')
+                    saver.save(sess, save_path='./checkpoint/vision_lang', global_step=model.global_step)
+
+            # training time
+            # time_interval = time.time() - start_time
+            # time_split = time.gmtime(time_interval)
+            # print("Training time: ", time_interval, "Hour: ", time_split.tm_hour, "Minute: ", time_split.tm_min,
+            #       "Second: ", time_split.tm_sec)
+            # for batch_image, batch_question, batch_question_length, batch_answer in batch_iterator(test_questions, args.batch_size):
+            #     gstep, _, test_loss, test_accuracy, test_l_summary, test_a_summary = sess.run([model.global_step, model.train_op, model.loss, model.accuracy, model.test_loss_summary, model.test_acc_summary], feed_dict={
+            #         model.image_input: batch_image,
+            #         model.question_input: batch_question,
+            #         model.question_length: batch_question_length,
+            #         model.target: batch_answer
+            #     })
+            #     # test_hit = np.array(np.argmax(test_y_pred, axis=1) == np.argmax(batch_answer, axis=1))
+            #     # test_acc = np.sum(test_hit) / hit.shape[0]
+            #     print('validation ' + str(epoch) + ' test_acc: ' + str(test_accuracy) + ' loss: ' + str(test_loss))
+            #
+            #     writer.add_summary(test_l_summary, gstep)
+            #     writer.add_summary(test_a_summary, gstep)
 
 
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    main(args)
